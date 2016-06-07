0832f7db799d4279e9da6316a73bfb482f261adf
Gabe Ratcliff <gaberatcliff@gmail.com>
2016-06-07 14:02:13 -0700

    updated port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, './client')));
 
 
 
-app.set('port', (process.env.PORT || 2020));
+app.set('port', (process.env.PORT || 8000));
 app.set('/', function (req, res){
 	res.send("<h1> Hello World </h1>");
 })
